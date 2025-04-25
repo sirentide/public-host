0a0e6c6ebbfa7d15153985365a787814bade39ef
Chainan Butkamwong <chainun_06@hotmail.com>
2025-04-26 02:59:46 +0700

    fix name

--- image-id/mapping.bat
+++ image-id/mapping.bat
@@ -2,13 +2,17 @@
 setlocal enabledelayedexpansion
 
 :: Initialize output files
+echo ID extract > id_extract.txt
+echo( >> id_extract.txt
+echo name Extract > name_extract.txt
+echo( >> name_extract.txt
 
 for %%F in (*.webp) do (
     set "filename=%%~nF"
     set "full=%%F"
 
-    :: Extract ID (1st token) and name (last token)
-    for /f "tokens=1,8 delims=_" %%A in ("!filename!") do (
+    :: Extract ID (1st token) and name (9th token)
+    for /f "tokens=1,9 delims=_" %%A in ("!filename!") do (
         set "id=%%A"
         set "name=%%B"
     )
